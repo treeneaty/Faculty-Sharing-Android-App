4c5b0de2e9812b080a48c46df8ca93984860dab6
Tabitha Murugi <tmurugi21@gmail.com>
2020-04-14 22:15:43 +0300

    Add Login Activity

---
+++ app/src/main/java/com/tabz/faculty/sharing/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.tabz.faculty.sharing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
